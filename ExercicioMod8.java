a50ad2cba65482e77607a9fce09634714e4d9b83
cauevinnicius <135167189+cauevinnicius@users.noreply.github.com>
2024-03-01 15:52:31 -0300

    Versão 2.0 - Correta
    Atualização para calculadora de médias. O exercício exigia apresentar 4 notas de um determinado aluno e realizar sua média.

--- ExercicioMod8.java
+++ ExercicioMod8.java
@@ -1,16 +1,19 @@
 public class ExercicioMod8 {
 
     public static void main (String [] args){
-    System.out.println("*** Calculadora de Idade ***");
-    CalculaIdade();
+    System.out.println("*** Calculadora de Notas ***");
+    CalculaNotas();
 }
 
-    private static void CalculaIdade() {
+    private static void CalculaNotas() {
         String nome = "Cauê";
-        int anoNasc = 1999;
-        int anoAtual = 2024;
-        System.out.println("Olá Sr. " + nome + "\nSeu ano de nascimento informado é: " + anoNasc + " sendo que você está em: " + anoAtual);
-        int calculaIdade = anoAtual - anoNasc;
-        System.out.println("Portanto, sua idade é " + calculaIdade + " anos!");
+        double nota1 = 8.0;
+        double nota2 = 9.5;
+        double nota3 = 7.0;
+        double nota4 = 9.1;
+        double calculaNotas = nota1 + nota2 + nota3 + nota4;
+        double resultado = calculaNotas/4;
+        System.out.println("Olá Sr. " + nome + "\nSuas notas informadas foram: \n" + nota1 + "\n" + nota2 + "\n" + nota3 + "\n" + nota4);
+        System.out.println("Portanto, sua média é " + resultado + ". Parabéns!");
     }
 }
